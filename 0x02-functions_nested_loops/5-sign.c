a3fdb76ac95ae5da708ef151f9a3d5652da27b25
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-03-21 16:03:52 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,22 +6,19 @@
  *
  *Return: 0 if zero, 1 if positive, -1 if negative
  */
-
 int print_sign(int n)
 {
-        if (n > 0)
-                {
-                _putchar(43);
-                return (1);
-                }
-        if (n < 0)
-        {
-                _putchar(45);
-                return (-1);
-        }
-        else
-        {
-                _putchar(48);
-                return (0);
-        }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
