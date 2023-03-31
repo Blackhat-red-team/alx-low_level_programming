416dc61d7f815a8bf8a62b353d1757fcad9ec650
Blackhat-red-team <75793444+Blackhat-red-team@users.noreply.github.com>
2023-04-01 00:29:37 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * _isupper - checks >>if parameter is an uppercase character.
+ * @c: input charac>ter.
+ * Return: 1 if is an upperc>ase character, 0 in other case.
  */
 int _isupper(int c)
 {
