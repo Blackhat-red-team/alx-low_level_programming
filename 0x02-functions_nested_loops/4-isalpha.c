682ffd678c74daa0fda539afc6192fa048afc3b8
Blackhat-red-team <75793444+Blackhat-red-team@users.noreply.github.com>
2023-04-01 00:17:28 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - checkss for alphabetic character
+ * @c: the characteer to be checkedd
+ * Return: 1 if c is a letter, 0 otherwises
  */
 int _isalpha(int c)
 {
