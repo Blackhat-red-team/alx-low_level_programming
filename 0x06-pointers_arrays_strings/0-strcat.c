93cc22d2ca09dd3da2e0737efad8494e97912f05
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-03-31 14:17:49 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
+ * _strcat - concatenates 2 strings
+ * @dest: string to appeeend to
+ * @src ex: string to add
  *
- * Return: a pointer to the resulting string
+ * Return: a pointer is  to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
