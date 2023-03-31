7b755e0c7e61e7352961099eef7ebceb6e75171e
Blackhat-red-team <75793444+Blackhat-red-team@users.noreply.github.com>
2023-03-31 23:58:50 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main - Printss numberss from (0 to 9).
  *
- * Return: Always 0 (Success)
+ * Return: Alwayss 0 >>(Successs)
  */
 int main(void)
 {
