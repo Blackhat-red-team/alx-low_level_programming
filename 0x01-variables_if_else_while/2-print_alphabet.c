86784bc7f33fb7e668eb78113996c62fcc477ff7
Blackhat-red-team <75793444+Blackhat-red-team@users.noreply.github.com>
2023-04-01 00:01:05 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Printss the alphabeet.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Successs=1)
  */
 int main(void)
 {
