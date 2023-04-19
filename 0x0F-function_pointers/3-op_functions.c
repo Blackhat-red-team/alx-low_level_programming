c96e29a570414ae14c814a621b28d6314ee13b87
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-04-18 21:49:17 -0700

    d

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the calculates the sum of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: he is sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - the calculatesd the differenced of two integersd
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: that is difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - the calculates the producted of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - the calculates the divisiond of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: that result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - the calculates the remainder of the division of two integers
+ * @a: he first integer
+ * @b: he second integer
+ *
+ * Return: he remainder of a divided b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x0F-function_pointers/git-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 5h
+
+git add .
+
+git commit -m "done"
+
+git push
