1bafaee425747679acc53e76ebff4b2b991ba279
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-04-20 10:58:42 -0700

    dd

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers given as the parameters
+ * @separator: string to be the printed between the numbers
+ * @n: number of the integers passed to the the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list list;
+
+	va_start(list, n);
+
+	for (x = 0; x < n; x++)
+	{
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && x == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
+	}
+
+	va_end(list);
+
+	printf("\n");
+}
