a99eaf4ce1d037fada921afde4927de898b33a70
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-04-20 10:52:25 -0700

    d

--- 0-sum_them_all.c
+++ 0-sum_them_all.c
@@ -8,13 +8,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int x;
+	unsigned int i;
 	int sum = 0;
 	va_list list;
 
-	va_start(list, n);
+	va_start(list, i);
 
-	for (x = 0; x < n; x++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(list, int);
 
 	va_end(list);
