dd3c8069c6dc5d78b44eb9f8f53fb639ed1bcec6
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-05-04 10:41:49 -0700

    complate

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - stdout is updated with the letter c
+ * @a: The character The text to be printed
+ *
+ * Return: On success 1.
+ * On error, Errno is set properly and -1 is returned
+ */
+int _putchar(char a)
+{
+	return (write(1, &a, 1));
+}
+
